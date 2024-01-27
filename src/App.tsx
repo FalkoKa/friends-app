8c43649b506c8fd37c8bd27573d2a935b849aa81
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2024-01-27 12:19:24 +0100

    Add fronts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import Card from './components/Card';
+import CardList from './components/CardList';
+import Scroll from './components/Scroll';
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState<string>('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {
+        setRobots(users);
+      });
+  }, []);
+
   return (
     <>
-      <div>
-        <Card />
+      <div className="tc">
+        <h1>Robofriends</h1>
+        <Scroll>
+          <CardList friends={robots} />
+        </Scroll>
       </div>
     </>
   );
